4dfa0038ef0ea3a39020ab091b33abfcae9f62e4
ysw <1779001867@qq.com>
2020-04-05 09:21:06 +0800

    每日一题

--- src/day05/Main.java
+++ src/day05/Main.java
@@ -1,6 +1,5 @@
 package day05;
 
-import java.util.Arrays;
 import java.util.Scanner;
 
 /**
@@ -10,39 +9,35 @@ import java.util.Scanner;
  * @Version
  **/
 public class Main {
+    static int count = 0;
+
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
         int n = input.nextInt();
-        int[] arr = new int[n];
-        for (int i = 0; i < n; i++) {
+        int[] arr = new int[n+1];
+        for (int i = 1; i <=n; i++) {
             arr[i] = input.nextInt();
         }
         input.close();
-        Arrays.sort(arr);
-        int count = 0;
-        int sum = 0;
-        for (int i = 0; i < n; i++) {
-            for (int j = i; j < n; j++) {
-                sum += arr[i] + arr[j];
-                if (sum == 40) {
-                    count++;
-                    continue;
-                }
-                int k = j;
-                while ((k+1 < n) && sum < 40) {
-                    sum += arr[k];
-                    if (sum == 40) {
-                        count++;
-                        continue;
-                    }
-                    if (sum > 40) {
-                        break;
-                    }
-                    k++;
-                }
-                sum = 0;
-            }
-        }
+        input.close();
+        count(arr,40,n);
         System.out.println(count);
+    }
+    /**
+     *
+     * @Param [arr, s, n] arr存放物品的权重，s表示剩余可以装物品的容量
+     *        n表示可以选择的物品个数
+     * @return void
+     **/
+    public static void count(int[] arr, int s, int n) {
+        if (s == 0) {
+            count++;
+            return;
+        }
+        if (s < 0 || (s > 0 && n < 1)) {
+            return;
+        }
+        count(arr, s - arr[n], n - 1);
+        count(arr, s, n - 1);
     }
 }
