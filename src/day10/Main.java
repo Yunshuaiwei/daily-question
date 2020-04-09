b86348550333c66cb197f72f0b4ffe8d13ad7057
ysw <1779001867@qq.com>
2020-04-09 17:29:56 +0800

    每日一题

---
+++ src/day10/Main.java
@@ -0,0 +1,19 @@
+package day10;
+
+/**
+ * @Description TODO
+ * @Author YunShuaiWei
+ * @Date 2020/4/9 16:01
+ * @Version
+ **/
+public class Main {
+    public static void main(String[] args) {
+
+    }
+    public int countWays(int x, int y) {
+        // write code here
+        int count=0;
+
+        return count;
+    }
+}
---
+++ src/day10/Main1.java
@@ -0,0 +1,30 @@
+package day10;
+
+import java.util.Scanner;
+
+/**
+ * @Description TODO
+ * @Author YunShuaiWei
+ * @Date 2020/4/9 16:32
+ * @Version
+ **/
+public class Main1 {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        int n = input.nextInt();
+        int[] a = new int[3];
+        a[0] = 0;
+        a[1] = 1;
+        while (true) {
+            int n1 = n - a[1];
+            a[2] = a[0] + a[1];
+            a[0] = a[1];
+            a[1] = a[2];
+            int n2 = n - a[2];
+            if (n2 < 0) {
+                break;
+            }
+        }
+        System.out.println(Math.min(n - a[0], a[1] - n));
+    }
+}
